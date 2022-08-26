7839e98cf6d8393d65593e352efa4fbbc33ec840
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-26 17:39:18 -0500

    [Add] the initial config in webpack and the initial files

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const rulesForCss = {
+  test: /\.scss$/i,
+  use: ["style-loader", "css-loader","sass-loader"],
+};
+
+const rulesForFiles = {
+  test: /\.(png|svg|jpg|jpeg|gif)$/i,
+  type: 'asset/resource',
+};
+
+
+const rulesForJs = {
+  test: /\.m?js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }
+};
+
+const rulesForHtml = {
+  test: /\.html$/i,
+  loader: "html-loader",
+}
+
+
+const rules = [rulesForCss, rulesForJs, rulesForFiles, rulesForHtml];
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    rules
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Wepack App',
+      filename: 'index.html',
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist')
+    },
+    open: true,
+    port: 3000,
+    compress: true, // enable gzip compression
+    client: {
+      overlay: { // show errors in browser
+        errors: true,
+        warnings: false,
+      },
+    },
+  }
+};
