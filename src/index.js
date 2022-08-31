3aeb75c17aa0e641f2a39ebbbc014cc646b75303
francisco-solis99 <franciscosoliscss@gmail.com>
2022-08-31 17:20:14 -0500

    [Add] the styles fo the header and navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles/styles.scss';
+
+
+const btnOpenMenu = document.querySelector('.nav__btn--open');
+const btnCloseMenu = document.querySelector('.nav__btn--close');
+const overlay = document.querySelector('.overlay');
+
+
+const menu = document.querySelector('.nav__list--primary');
+
+btnOpenMenu.addEventListener('click', toggleMenu);
+btnCloseMenu.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
