0acc8d10cd30e66dfe1fc37d5f50b7648334db50
francisco-solis99 <franciscosoliscss@gmail.com>
2022-09-12 15:33:21 -0500

    [Add] the slider for desktop and mobile view

--- src/index.js
+++ src/index.js
@@ -15,3 +15,42 @@ function toggleMenu() {
   menu.classList.toggle('active');
   overlay.classList.toggle('active');
 }
+
+// Slider
+let indexSlides = 0;
+const [btnSliderPrev, btnSliderNext] = document.querySelectorAll('.slider__control')
+const slider = document.querySelector('.slider__container');
+const numSlides = document.querySelectorAll('.slider__item').length;
+const thumbnailWrapper = document.querySelector('.slider__thubmbails');
+
+
+btnSliderNext.addEventListener('click', () => {
+  indexSlides += 1;
+  handlerSlider();
+});
+
+btnSliderPrev.addEventListener('click', () => {
+  indexSlides -= 1;
+  handlerSlider();
+});
+
+
+function handlerSlider() {
+  if (indexSlides < 0) indexSlides = numSlides - 1;
+  if (indexSlides > numSlides - 1) indexSlides = 0;
+  slider.style.transform = `translate(-${100 * indexSlides}%)`;
+}
+
+
+// Thumbnails preview
+thumbnailWrapper.addEventListener('click', (e) => handlerThumbnail(e))
+let previousThumbnail = document.querySelector('.slider__thumb-item');
+function handlerThumbnail(e) {
+  if (!e.target.classList.contains('slider__thumb-item')) return;
+
+  indexSlides = e.target.dataset.thumbIndex;
+  previousThumbnail?.classList.remove('active');
+  e.target.classList.add('active');
+  previousThumbnail = e.target;
+  handlerSlider();
+}
